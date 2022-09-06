d8126198705221347ad1dfa5060205fbedb22226
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-09-06 22:03:25 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -19,42 +19,42 @@ function clearCountriesData() {
   refs.infoEl.innerHTML = '';
 }
 
-// function buildListOnPage(e) {
-//   const list = makeCountryListMarkup(e);
-//   refs.listEl.insertAdjacentHTML("beforeend", list);
-// }
+function buildListOnPage(e) {
+  const list = makeCountryListMarkup(e);
+  refs.listEl.insertAdjacentHTML("beforeend", list);
+}
 
-// function buildInfoOnPage(e) {
-//   const info = makeCountryInfoMarkup(e);
-//   refs.infoEl.insertAdjacentHTML("beforeend", info);
-// }
+function buildInfoOnPage(e) {
+  const info = makeCountryInfoMarkup(e);
+  refs.infoEl.insertAdjacentHTML("beforeend", info);
+}
 
-// function onInputSearch(event) {
-//   clearCountriesData();
-//     let searchName = event.target.value.trim();
-//   if (!searchName) {
-//     return;
-//   }
-//   fetchCountries(searchName)
-//     .then(el => {
-//       if (el.length > 10) {
-//         return countriesLengthCheck();
-//       }
-//       if (el.length > 1 && el.length <= 10) {
-//         clearCountriesData();
-//         buildListOnPage(el);
-//       } else {
-//         clearCountriesData();
-//         buildInfoOnPage(el);
-//       }
-//     })
-//     .catch(onFetchError);
-// }
+function onInputSearch(event) {
+  clearCountriesData();
+    let searchName = event.target.value.trim();
+  if (!searchName) {
+    return;
+  }
+  fetchCountries(searchName)
+    .then(el => {
+      if (el.length > 10) {
+        return countriesLengthCheck();
+      }
+      if (el.length > 1 && el.length <= 10) {
+        clearCountriesData();
+        buildListOnPage(el);
+      } else {
+        clearCountriesData();
+        buildInfoOnPage(el);
+      }
+    })
+    .catch(onFetchError);
+}
 
-// function countriesLengthCheck() {
-//   return Notify.info('Too many matches found. Please enter a more specific name.');
-// }
+function countriesLengthCheck() {
+  return Notify.info('Too many matches found. Please enter a more specific name.');
+}
 
-// function onFetchError() {
-//   return Notify.failure("Oops, there is no country with that name");
-// }
+function onFetchError() {
+  return Notify.failure("Oops, there is no country with that name");
+}
